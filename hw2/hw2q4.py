d81eab471d788eda995036ea7555932b974d8a39
kolluashrith <akollu1@jh.edu>
2026-09-24 01:49:10 -0400

    fixed hw2q4 algorithm and added the test files

--- hw2/hw2q4.py
+++ hw2/hw2q4.py
@@ -1,82 +1,91 @@
 import sys
 
 def verify_position(startpos, read_length, ref_length):
+    """Function to verify that the given starting position is a valid position by checking against reference sequence ranges"""
     if (startpos < 0 or startpos + read_length > ref_length):
         return False
     else:
         return True
 
-def check_read(read, reference_dict, reference_seq):
-    offset_matches = []
+def check_pos(pos, read, reference_seq):
+    """Function to check if the given position could be the start to a valid alignment"""
+
+    #Verify that alignment is possible
+    mismatch_possible = verify_position(pos, len(read), len(reference_seq))
+
+    mismatches = 0
+    num_matched_bases_without_mismatch = 0
+
+    #Check exact match or single mismatched pair
+    if (mismatch_possible):
+        for l in range(len(read)):
+            if (read[l] != reference_seq[pos + l]):
+                mismatches += 1
+            elif mismatches == 0:
+                num_matched_bases_without_mismatch += 1
+            else:
+                continue
+
+            if mismatches > 1:
+                break
+
+        #Return yes if we have an exact match or one-off match
+        if mismatches < 2:
+            return True
     
+    #Now, we check for deletions in the read
+    
+    deletion_possible = pos + len(read) + 1 <= len(reference_seq) #check that we can read one additional base
+
+    if (deletion_possible):
+        temp = num_matched_bases_without_mismatch
+        for m in range(num_matched_bases_without_mismatch, len(read)):
+            if (read[m] != reference_seq[pos + m + 1]):
+                break
+            else:
+                num_matched_bases_without_mismatch += 1
+
+            #Return yes if we have an single-deletion
+        if num_matched_bases_without_mismatch >= len(read) - 1:
+            return True
+        else:
+            num_matched_bases_without_mismatch = temp #reset
+
+    #Now check insertion in read if needed. Make sure we can read there
+
+    insertion_possible = verify_position(pos, len(read) - 1, len(reference_seq)) #less strict than single mismatch base
+
+    if (insertion_possible):
+        for g in range(num_matched_bases_without_mismatch + 1, len(read)):
+            if (read[g] != reference_seq[pos + g - 1]):
+                return False
+        return True
+    else:
+        return False
+
+
+def check_read(read, reference_dict, reference_seq):
+    """Checks to see if the specified read can be found within one edit distance in the reference sequence, given the kmer dictionary"""
+
     for i in range(2):
         offset_array = []
         read_segment = read[i*10:i*10+10]
 
-        #Get array of possible starts
+        #Get array of possible matches
         if (read_segment in reference_dict):
             offset_array = reference_dict[read_segment]
 
         for offset_candidate in offset_array:
 
-            mismatches = 0
-            num_matched_bases = 0
-            startpos = offset_candidate - i*10
+            startpos = [offset_candidate - i*10 - 1, offset_candidate - i*10, offset_candidate - i*10 + 1]
 
-            #Avoid rechecking found matches
-            if startpos in offset_matches:
-                continue
+            for pos in startpos:
 
-            #Verify that alignment is possible
-            if not verify_position(startpos, len(read), len(reference_seq)):
-                continue
-
-
-            #First, check for full match or single mismatch
-            mismatch_or_good = True
-
-            for l in range(len(read)):
-                if (read[l] != reference_seq[startpos + l]):
-                    mismatches += 1
-                elif mismatches == 0:
-                    num_matched_bases += 1
+                if check_pos(pos, read, reference_seq):
+                    return True
                 else:
                     continue
-
-                if mismatches > 1:
-                    mismatch_or_good = False
-                    break
-
-            #Now, we check for deletions in the read
-            deletion = True
-
-            if (startpos + len(read) + 1 <= len(reference_seq)):
-                deletion = False
-
-            if (not mismatch_or_good and deletion):
-                for m in range(num_matched_bases, len(read)):
-                    if (read[m] != reference_seq[startpos + m + 1]):
-                        deletion = False
-                        break
-            
-
-            #Now check insertion if needed. Make sure we can read there
-            insertion = True
-            if not verify_position(startpos, len(read) - 1, len(reference_seq)):
-                insertion = False
-            
-            if (not mismatch_or_good and not deletion and insertion):
-                for m in range(num_matched_bases+1, len(read)):
-                    if (read[m] != reference_seq[startpos + m - 1]):
-                        insertion = False
-                        break
-
-            if ((mismatch_or_good or insertion or deletion) and (startpos not in offset_matches)):
-                offset_matches.append(startpos)
-
-    return offset_matches
-
-
+    return False
 
 def main(FASTA_filename, FASTQ_filename, output_filename):
     #Store all arguments from command line
@@ -124,14 +133,13 @@ def main(FASTA_filename, FASTQ_filename, output_filename):
     #Read FASTQ and perform alignment
     for _ in FASTQ_file:
         read = FASTQ_file.readline().strip() #throw out first, read second
+        if read == "":
+            continue
 
-        offset_matches = check_read(read, ref_kmer_dict, referenceseq)
+        match = int(check_read(read, ref_kmer_dict, referenceseq))
 
-        #Write every good offset in order
-        offset_matches.sort()
-        for num in offset_matches:
-            output_file.write(f"{num} ")
-        output_file.write("\n")
+        #Write 1 if found
+        output_file.write(f"{match}\n")
 
         #Throw out lines 3 and 4
         FASTQ_file.readline()
@@ -144,7 +152,7 @@ def main(FASTA_filename, FASTQ_filename, output_filename):
 
     return
 
-
+#Cody body that runs once command line calls script
 FASTA_filename = sys.argv[1]
 FASTQ_filename = sys.argv[2]
 output_filename = sys.argv[3]
